a0d1f935f52c2ebefeb7fb8544e6b48a72ca4825
Arjun Variar <Arjun.Variar@airtel.com>
2020-07-13 20:25:55 +0530

    Adding command line flags

--- main.cpp
+++ main.cpp
@@ -1,30 +1,57 @@
 #include "src/esola.cpp"
+#include "src/argparse.h"
 #include <chrono>
 
 using namespace std;
 using namespace kfr;
+using namespace argparse;
 
-univector<float> convert_audio_to_float(univector<int16_t> audio) {
-    univector<float> float_audio;
-    for (int i = 0; i < audio.size(); ++i) {
-        float_audio.push_back(float(audio[i]) / 32768.0);
+int parse(ArgumentParser& parser, int argc, const char* argv[]) {
+    parser.add_argument()
+            .names({"-t", "--time_scale"})
+            .required(true);
+    parser.add_argument()
+            .names({"-i", "--input_file"})
+            .required(true);
+    parser.add_argument()
+            .names({"-o", "--output_file"})
+            .required(true);
+    parser.enable_help();
+    auto err = parser.parse(argc, argv);
+    if (err) {
+        std::cout << err << std::endl;
+        return -1;
     }
-    return float_audio;
+    if (parser.exists("help")) {
+        parser.print_help();
+        return 0;
+    }
+    return 1;
 }
 
-int main() {
-    std::string filename("/Users/vigi99/AirtelDEV/VoiceDataForAirtel/CommonCorpusTest/sample-010955.wav");
-    audio_reader_wav<int16_t> reader(open_file_for_reading(filename));
-    univector2d<int16_t> audio = reader.read_channels();
-    const std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    auto input_audio = convert_audio_to_float(audio[0]);
-    auto output_audio = esola(input_audio, float(1.0/0.75), 3, reader.format().samplerate);
-    const std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    spdlog::info("Final Length = {}, ratio = {}", output_audio.size(),  float(output_audio.size()) / float(reader.format().length));
+int main(int argc, const char* argv[]) {
+    ArgumentParser parser("esola", "ESOLA C++");
+    int code = parse(parser, argc, argv);
+    if (code != 1) {
+        return code;
+    }
+    auto input_filepath = parser.get<std::string>("input_file");
+    auto output_filepath = parser.get<std::string>("output_file");
+    auto time_scaling_factor = parser.get<float>("time_scale");
 
-    audio_writer_wav<f32> writer(open_file_for_writing(KFR_FILEPATH("/Users/vigi99/Downloads/1_5_scope.wav")),
+    audio_reader_wav<float> reader(open_file_for_reading(input_filepath));
+    univector2d<float> audio = reader.read_channels();
+    const std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    auto input_audio = audio[0];
+    auto output_audio = esola(input_audio, float(1.0/time_scaling_factor), 3, reader.format().samplerate);
+    const std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    audio_writer_wav<f32> writer(open_file_for_writing(KFR_FILEPATH(output_filepath)),
                                    audio_format{ 1, audio_sample_type::i16, reader.format().samplerate, false });
     writer.write(output_audio.data(), output_audio.size());
+
+    spdlog::info("Final Length = {}, ratio = {}", output_audio.size(),  float(reader.format().length) / float(output_audio.size()));
     std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
     return 0;
 }
