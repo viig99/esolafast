d4222a973b61c7fe10a5bcd99796611beed0d63e
Arjun Variar <Arjun.Variar@airtel.com>
2020-07-20 13:20:02 +0530

    Adding other libraries for python tests.

--- examples/test.py
+++ examples/test.py
@@ -2,8 +2,21 @@ import sys
 sys.path += ["build/lib.macosx-10.15-x86_64-3.8"]
 from esolafast import time_stretch
 import soundfile as sf
+from time import process_time
+import sox
 
-audio, samplerate = sf.read(INPUT_PATH, dtype='float32')
-output_audio = time_stretch(audio, 1.2, samplerate)
+rate = 1.5
+input_path = ""
+output_path = ""
 
-sf.write(OUTPUT_PATH, output_audio, samplerate)
+audio, samplerate = sf.read(input_path, dtype='float32')
+start = process_time()
+output_audio = time_stretch(audio, rate, samplerate)
+print(f"Time taken for esolafast was {(process_time()-start)*1000:.2f}ms")
+sf.write(output_path, output_audio, samplerate)
+
+tfm = sox.Transformer()
+tfm.stretch(rate)
+start = process_time()
+array_out = tfm.build_array(input_filepath=input_path)
+print(f"Time taken for sox was {(process_time()-start)*1000:.2f}ms")
